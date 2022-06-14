c5ccb2ecb5ee221e701e6a194543cd7eb846e4e8
Vadym Naumenko <73651247+vda132@users.noreply.github.com>
2022-06-14 14:21:37 +0300

    task 5 completed

---
+++ Task5/Task5/Task5/Program.cs
@@ -0,0 +1,21 @@
+﻿//TODO Даны два целых числа A и В.
+//Выведите все числа от A до B включительно, в порядке возрастания, если A < B, или в порядке убывания в противном случае. Использовать рекурсию.
+
+Recursia(2, 9);
+Recursia(10, 4);
+
+void Recursia(int a, int b)
+{
+    if (a < b)
+    { 
+        Recursia(a, b - 1);
+        Console.WriteLine(b);
+    }
+    else if (a > b)
+    {
+        Console.WriteLine(a);
+        Recursia(a - 1, b);
+    }
+    else
+        Console.WriteLine(a);
+}
