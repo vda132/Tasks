a197e28b0c4e3c470b467a2cdcc52ddb65495806
Vadym Naumenko <73651247+vda132@users.noreply.github.com>
2022-06-14 12:33:21 +0300

    task 2 completed

---
+++ Task2/Task2/Task2/Person.cs
@@ -0,0 +1,48 @@
+﻿namespace Task2;
+
+public class Person
+{
+    private string fullname = string.Empty;
+    private int age;
+
+    public string Fullname
+    {
+        get => fullname;
+
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+                return;
+
+            fullname = value;
+        }
+    }
+
+    public int Age
+    {
+        get => age;
+        set
+        {
+            if (value > 0 && value < 100)
+                age = value;
+        }
+    }
+
+    public Person()
+    {
+        Fullname = "John";
+        Age = 30;
+    }
+
+    public Person(string fullname, int age)
+    {
+        Fullname = fullname;
+        Age = age;
+    }
+
+    public void Move() =>
+        Console.WriteLine($"{this.Fullname} идет");
+
+    public void Talk() =>
+        Console.WriteLine($"{this.Fullname} говорит");
+}
---
+++ Task2/Task2/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// TODO: Создать класс Person, который содержит: 
+// переменные fullName, age;
+// методы move() и talk(), в которых просто вывести на консоль сообщение -"Такой-то  Person говорит". 
+// Добавьте два конструктора  - Person() и Person(fullName, age).
+// Создайте два объекта этого класса. Один объект инициализируется конструктором Person(), другой - Person(fullName, age).
+
+using Task2;
+
+var person1 = new Person();
+var person2 = new Person("Jack", 19);
+
+person1.Talk();
+person2.Move();
