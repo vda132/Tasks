738cd7969bad41cb1504fed35b92199bb9d29671
Vadym Naumenko <73651247+vda132@users.noreply.github.com>
2022-06-14 14:40:28 +0300

    task 6 completed

---
+++ Task6/Task6/Task6/Aspirant.cs
@@ -0,0 +1,24 @@
+﻿namespace Task6;
+
+public class Aspirant : Student
+{
+    private string scienceWork = string.Empty;
+
+    public string ScienceWork
+    {
+        get => scienceWork;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+                return;
+
+            scienceWork = value;
+        }
+    }
+
+    public Aspirant(string firstname, string lastname, string group, double avarageMark, string scienceWork) : base(firstname, lastname, group, avarageMark) =>
+        ScienceWork = scienceWork;
+
+    public override int GetScholarShip() =>
+        AvarageMark == 5 ? 200 : 180;
+}
---
+++ Task6/Task6/Task6/Student.cs
@@ -0,0 +1,62 @@
+﻿namespace Task6;
+
+public class Student
+{
+    protected string firstname = string.Empty;
+    protected string lastname = string.Empty;
+    protected string group = string.Empty;
+    protected double avarageMark;
+
+    public string Firstname
+    {
+        get => firstname;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+                return;
+            firstname = value;
+        }
+    }
+
+    public string Lastname
+    {
+        get => lastname;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+                return;
+            lastname = value;
+        }
+    }
+
+    public string Group
+    {
+        get => group;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+                return;
+            group = value;
+        }
+    }
+    public double AvarageMark
+    {
+        get => avarageMark;
+        set
+        {
+            if(value > 0 && value <=5)
+                avarageMark = value;
+        }
+    }
+
+    public Student(string firstname, string lastname, string group, double avarageMark)
+    {
+        Firstname = firstname;
+        Lastname = lastname;
+        Group = group;
+        AvarageMark = avarageMark;
+    }
+
+    public virtual int GetScholarShip() =>
+        avarageMark == 5 ? 100 : 80;
+}
